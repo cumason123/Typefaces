cdb8d61f4308eb4795a490245b8151928f9afd6a
Bianca Oparaji <chinwebiancaoparaji@biancas-air.attlocal.net>
2018-05-22 22:10:50 -0500

    Added Learnin Rate Evaluator learning algorithm for cnn

--- src/KerasClass/KerasClass.py
+++ src/KerasClass/KerasClass.py
@@ -2,9 +2,12 @@ from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Dense, Flatten
 from keras.models import Sequential
 from keras.models import load_model
+from keras.optimizers import SGD
+from keras.callbacks import LearningRateScheduler, ModelCheckpoint
 
 
-class CNN:
+
+class cnn:
 
     def __init__(self, input_dim):
         model = Sequential()
@@ -19,6 +22,27 @@ class CNN:
 
         self.cnn = model
 
+        lr = 0.01
+        sgd = SGD(lr=lr, decay=1e-6, momentum=0.9, nesterov=True)
+        model.compile(loss='categorical_crossentropy',
+                      optimizer=sgd,
+                      metrics=['accuracy'])
+
+
+def learn_rate(epoch):
+    return lr * (0.1 ** int(epoch / 10))
+
+batch_size = 32
+epochs = 30
+
+model.fit(X, Y,
+        batch_size=batch_size,
+        epochs=epochs,
+        validation_split=0.2,
+        callbacks=[LearningRateScheduler(lr_schedule),
+                    ModelCheckpoint('model.h5', save_best_only=True)]
+        )
+
 
     def save_model(self):
         self.model.save('model.h5')
@@ -27,5 +51,6 @@ class CNN:
 
     def load_model(self):
         self.model = load_model('model.h5')  # type: object
+
 
 
